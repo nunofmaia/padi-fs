0bb3b73a50d62cb2f4cc51bfac105f952196f203
Nuno Maia <nunofmaia@gmail.com>
2013-03-20 18:06:43 +0000

    Added Client interface.

---
+++ padiFS/CommonTypes/IClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace padiFS
+{
+    public interface IClient
+    {
+        void Read(string filename);
+        void Write(string filename);
+        void Open(string filename);
+        void Create(string filename, int serversNumber, int readQuorum, int writeQuorum);
+    }
+}
