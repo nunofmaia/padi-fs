2c5c6179999f9495c756ca2d47009a9fe86b6e5e
Nuno Maia <nunofmaia@gmail.com>
2013-03-22 19:26:57 +0000

    Added new data type MetadataInfo for data exchanges between metadata servers.

---
+++ padiFS/CommonTypes/MetadataInfo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace padiFS
+{
+    [Serializable]
+    public class MetadataInfo
+    {
+        private Dictionary<string, string> liveDataServers;
+        private Dictionary<string, string> deadDataServers;
+        private Dictionary<string, int> serversLoad;
+        private Dictionary<string, Metadata> files;
+        private Dictionary<string, Metadata> openFiles;
+
+        public MetadataInfo(Dictionary<string, string> liveDataServers, Dictionary<string, string> deadDataServers,
+            Dictionary<string, int> serversLoad, Dictionary<string, Metadata> files, Dictionary<string, Metadata> openFiles)
+        {
+            this.liveDataServers = liveDataServers;
+            this.deadDataServers = deadDataServers;
+            this.serversLoad = serversLoad;
+            this.files = files;
+            this.openFiles = openFiles;
+        }
+
+        public Dictionary<string, string> LiveDataServers
+        {
+            get
+            {
+                return liveDataServers;
+            }
+        }
+
+        public Dictionary<string, string> DeadDataServers
+        {
+            get
+            {
+                return deadDataServers;
+            }
+        }
+
+        public Dictionary<string, int> ServersLoad
+        {
+            get
+            {
+                return serversLoad;
+            }
+        }
+
+        public Dictionary<string, Metadata> Files
+        {
+            get
+            {
+                return files;
+            }
+        }
+
+        public Dictionary<string, Metadata> OpenFiles
+        {
+            get
+            {
+                return openFiles;
+            }
+        }
+    }
+}
