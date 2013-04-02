8f604e98cafefad40637cf3df6b00ccea9a32f4a
Antonio Goulao <goulao80.3@gmail.com>
2013-04-02 13:14:30 +0100

    Stores files in register when creating or opening them. Reads to string registers and writes from them or from a string provided by the script. Read method got some refactoring to avoid code repetition.

--- padiFS/Client/Client.cs
+++ padiFS/Client/Client.cs
@@ -44,7 +44,11 @@ namespace padiFS
             {
                 myFiles.Add(filename, meta);
                 openFiles.Add(filename, meta);
+
+                AddToFileRegister(meta);
                 Console.WriteLine("Create file " + filename);
+
+
             }
             else
             {
@@ -61,6 +65,7 @@ namespace padiFS
                 if (!openFiles.ContainsKey(filename))
                 {
                     openFiles.Add(filename, meta);
+                    AddToFileRegister(meta);
                 }
                 Console.WriteLine("Open file " + filename);
             }
@@ -71,6 +76,20 @@ namespace padiFS
 
         }
 
+        // Method used to add a new/open file to a free file register
+        // Limit - 10
+        private void AddToFileRegister(Metadata meta)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (fileRegister[i] == null)
+                {
+                    fileRegister[i] = meta;
+                }
+            }
+        }
+
+        // Method used by one thread to perform the call to the data servers
         private void ReadCallback(object threadcontext)
         {
             List<object> args = (List<object>)threadcontext;
@@ -101,6 +120,7 @@ namespace padiFS
             }
         }
 
+        // Method used by one thread to perform the Read operation
         private void ExecuteRead(string filename, string semantic)
         {
             if (openFiles.ContainsKey(filename))
@@ -112,55 +132,14 @@ namespace padiFS
 
                 // Call all the data servers that have the file and wait for a majority
                 // Launch threads and wait for it. Compare the answers and return it.
-                int i = 0;
-                int files = servers.Count;
-                foreach (string s in servers)
-                {
-                    List<object> arguments = new List<object>();
-                    arguments.Add(servers[i]);
-                    arguments.Add(filename);
-                    arguments.Add(semantic);
-                    ThreadPool.QueueUserWorkItem(ReadCallback, arguments);
-                    i++;
-                }
+                ReadCallDataServers(filename, semantic, servers);
+
                 Dictionary<DateTime, File> received = null;
                 Dictionary<DateTime, int> votes = null;
                 int winner = -1;
 
-                while (winner < readQuorum)
-                {
+                ReadVoting(readQuorum, ref received, ref votes, ref winner);
 
-                    // In the best case, all replies are right
-                    // In the worst case, this cycle is useless
-                    while (readFiles.Count < readQuorum)
-                    {
-                    }
-
-                    votes = new Dictionary<DateTime, int>();
-                    received = new Dictionary<DateTime, File>();
-
-                    // Count votes
-                    foreach (File f in readFiles)
-                    {
-                        if (!votes.ContainsKey(f.Version))
-                        {
-                            votes.Add(f.Version, 1);
-                            received.Add(f.Version, f);
-                        }
-                        else
-                        {
-                            votes[f.Version]++;
-                        }
-                    }
-
-                    // Sort votes and show the most voted
-                    votes = Util.SortVotes(votes);
-                    
-                    
-                    winner = votes.Values.Last();
-                    Console.WriteLine("Winner " + winner);
-                    Thread.Sleep(1000);
-                }
                 File selected = received[votes.Keys.Last()];
 
                 if (semantic.Equals("default"))
@@ -184,16 +163,131 @@ namespace padiFS
                     }
                     else
                     {
+                        historic.Add(filename, selected);
                         Console.WriteLine("Read file " + filename + ": " + Util.ConvertByteArrayToString(selected.Content));
                     }
                 }
             }
         }
 
+        // Method that performs the voting count of answers from data servers
+        private void ReadVoting(int readQuorum, ref Dictionary<DateTime, File> received, ref Dictionary<DateTime, int> votes, ref int winner)
+        {
+            while (winner < readQuorum)
+            {
+
+                // In the best case, all replies are right
+                // In the worst case, this cycle is useless
+                while (readFiles.Count < readQuorum)
+                {
+                }
+
+                votes = new Dictionary<DateTime, int>();
+                received = new Dictionary<DateTime, File>();
+
+                // Count votes
+                foreach (File f in readFiles)
+                {
+                    if (!votes.ContainsKey(f.Version))
+                    {
+                        votes.Add(f.Version, 1);
+                        received.Add(f.Version, f);
+                    }
+                    else
+                    {
+                        votes[f.Version]++;
+                    }
+                }
+
+                // Sort votes and show the most voted
+                votes = Util.SortVotes(votes);
+
+
+                winner = votes.Values.Last();
+                Console.WriteLine("Winner " + winner);
+                Thread.Sleep(1000);
+            }
+        }
+
+        // Method that perform calls to all data servers that store the file
+        private void ReadCallDataServers(string filename, string semantic, List<string> servers)
+        {
+            int i = 0;
+            int files = servers.Count;
+            foreach (string s in servers)
+            {
+                List<object> arguments = new List<object>();
+                arguments.Add(servers[i]);
+                arguments.Add(filename);
+                arguments.Add(semantic);
+                ThreadPool.QueueUserWorkItem(ReadCallback, arguments);
+                i++;
+            }
+        }
+
+        // Read for Puppet Master GUI 
         public void Read(string filename, string semantic)
         {
             Thread t = new Thread(() => ExecuteRead(filename, semantic));
             t.Start();
+        }
+
+        // Read for Puppet Master Scripts
+        public void Read(int file, string semantic, int register)
+        {
+            Thread t = new Thread(() => ExecutePMRead(file, semantic, register));
+            t.Start();
+        }
+
+        private void ExecutePMRead(int file, string semantic, int register)
+        {
+            Metadata m = fileRegister[file];
+            string filename = m.FileName;
+            List<string> servers = m.DataServers;
+            int readQuorum = m.ReadQuorum;
+            readFiles = new ConcurrentBag<File>();
+
+            // Call all the data servers that have the file and wait for a majority
+            // Launch threads and wait for it. Compare the answers and return it.
+            ReadCallDataServers(filename, semantic, servers);
+
+            Dictionary<DateTime, File> received = null;
+            Dictionary<DateTime, int> votes = null;
+            int winner = -1;
+
+            ReadVoting(readQuorum, ref received, ref votes, ref winner);
+
+            File selected = received[votes.Keys.Last()];
+
+            if (semantic.Equals("default"))
+            {
+                historic.Add(filename, selected);
+                stringRegister[register] = selected.Content;
+                Console.WriteLine("Read file " + filename + ": " + Util.ConvertByteArrayToString(selected.Content));
+            }
+            else
+            {
+                if (historic.ContainsKey(filename))
+                {
+                    File h = historic[filename];
+                    if (selected.Version > h.Version)
+                    {
+                        stringRegister[register] = selected.Content;
+                        Console.WriteLine("Read file " + filename + ": " + Util.ConvertByteArrayToString(selected.Content));
+                    }
+                    else
+                    {
+                        stringRegister[register] = selected.Content;
+                        Console.WriteLine("Read file " + filename + ": " + Util.ConvertByteArrayToString(h.Content));
+                    }
+                }
+                else
+                {
+                    historic.Add(filename, selected);
+                    stringRegister[register] = selected.Content;
+                    Console.WriteLine("Read file " + filename + ": " + Util.ConvertByteArrayToString(selected.Content));
+                }
+            }
         }
 
         private void WriteCallback(object threadcontext)
@@ -258,6 +352,38 @@ namespace padiFS
         {
             Thread t = new Thread(() => ExecuteWrite(filename, bytearray));
             t.Start();
+        }
+
+        public void Write(int file, int register)
+        {
+            Thread t = new Thread(() => ExecutePMWriteRegister(file, register));
+            t.Start();
+        }
+
+        public void Write(int file, string content)
+        {
+            Thread t = new Thread(() => ExecutePMWriteContent(file, content));
+            t.Start();
+        }
+
+        // Method to get the file from the file register and write the content
+        // provided in the script
+        private void ExecutePMWriteContent(int file, string content)
+        {
+            Metadata m = fileRegister[file];
+            string filename = m.FileName;
+            byte[] bytearray = Util.ConvertStringToByteArray(content);
+            ExecuteWrite(filename, bytearray);
+        }
+
+        // Method to get the file from the file register and write the content
+        // previously stored in the client's string register
+        private void ExecutePMWriteRegister(int file, int register)
+        {
+            Metadata m = fileRegister[file];
+            string filename = m.FileName;
+            byte[] bytearray = stringRegister[register];
+            ExecuteWrite(filename, bytearray);
         }
 
         public void Close(string filename)
