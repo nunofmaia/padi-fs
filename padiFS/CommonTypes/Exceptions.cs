a6d4409cdb7deaf0fea4a9cd7ba11192608a43b2
Nuno Maia <nunofmaia@gmail.com>
2013-04-04 10:57:02 +0100

    Client is now able to execute scripts. Added exceptions file to the project.

---
+++ padiFS/CommonTypes/Exceptions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace padiFS
+{
+    [Serializable]
+    public class PadiFsException : ApplicationException
+    {
+    }
+
+    public class FileNotFoundException : PadiFsException
+    {
+    }
+
+    public class ServerNotAvailable : PadiFsException
+    {
+    }
+
+    public class FileAlreadyOpen : PadiFsException
+    {
+    }
+
+    public class FileAlreadyClose : PadiFsException
+    {
+    }
+
+    public class ServerNotFound : PadiFsException
+    {
+    }
+}
